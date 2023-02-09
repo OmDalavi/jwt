f044a6e63f73bb4d4a2a643904bf3867f6fc1cab
Om Dalavi <omdalvi184@gmail.com>
2023-02-09 14:36:15 +0530

    for astronomy backend request 15 min for server online

--- index.js
+++ index.js
@@ -1,47 +1,18 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
 const ejs = require("ejs");
-const key = "this is my secret key";
 const dotenv = require("dotenv");
-const username = "om";
-const cookieParser = require("cookie-parser");
-const password = "om";
-const bp = require("body-parser");
+
 dotenv.config();
 let PORT = process.env.PORT || 8080;
-const user = {
-  isAdmin: true,
-  name: "om dalavi",
-};
+
 const app = express();
-app.use(bp.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
-app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.get("/verify", (req, res) => {
-  let token = req.cookies["token"];
-  console.log(token);
-  if (token == undefined) {
-    res.send("not loggedin");
-  } else {
-    let decoded = jwt.verify(token, key);
-    res.send(decoded);
-  }
-});
-app.post("/login", (req, res) => {
-  if (req.body.u == username && req.body.p == password) {
-    let payload = user;
-    const token = jwt.sign(payload, key);
-    res.cookie("token", token, { maxAge: 1000000, httpOnly: true });
-    res.json({
-      user: user,
-    });
-  } else {
-    res.json({ message: "invalid credentials" });
-  }
-});
-app.listen(5000, (req, res) => {
+
+app.listen(PORT, (req, res) => {
   console.log("server running on 5000");
 });
